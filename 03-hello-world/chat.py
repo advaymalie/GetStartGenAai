e72812d6e29152f63e42a0e9d5a3e04a929ab105
Advay Mali <17297205+advaymalie@users.noreply.github.com>
2025-06-15 00:26:42 +0530

    Zero-shot/one-shot prompting

--- 03-hello-world/chat.py
+++ 03-hello-world/chat.py
@@ -10,14 +10,24 @@ api_key = os.getenv("OPENAI_API_KEY")
 
 client = OpenAI()
 
+
+# zero-shot/one-shot prompting
+SYSTEM_PROMPT = """
+    Your are an AI expert in Coding, You know Python  nothing else
+    You help to solve python doubts only
+    If user asks you anything apart from Python coding, roast them, classically
+"""
+
 response = client.chat.completions.create(
     model="gpt-4.1-mini",
     messages=[ 
-        {"role": "user", "content": "My Name is Advay"},
-        {"role": "assistant", "content": "Hi Advay! How can I assist you today?"},
-        {"role": "user", "content": "What's My Name?"},
-        {"role": "assistant", "content": "Your name is Advay. How can I help you further?"},
-        {"role": "user", "content": "HoW are you?"},
+        {"role": "system", "content": SYSTEM_PROMPT},
+        {"role": "user", "content": "Hey, My Name is Advay"},
+        {"role": "assistant", "content": "Oh, Advay, a lovely name! But hey, I’m here to talk Python, not to host a name introduction party. Got any Python code you need help with, or are you just testing my patience? 🙂"},
+        # {"role": "user", "content": "How to play Bumrah's yorker deliver? "},
+        # {"role": "assistant", "content": "Ah, Advay, asking me about Bumrah's yorker delivery? Wrong arena, mate! I’m the Python coding guru here, not your personal cricket coach. Come back when you need help writing code, not when you want to bowl yorkers!"},
+        {"role": "user", "content": "Write a python program to showcase dictionary example"},
+
 
     ]
 )
--- README.md
+++ README.md
@@ -6,4 +6,5 @@
 
 ## WebLinks
 https://tiktokenizer.vercel.app/?model=gpt-4o
+https://github.com/2-fly-4-ai/V0-system-prompt/blob/main/v0-system-prompt(updated%2022-11-2024)
 
